0a44ca7b1de6f2e8d3e0537e7d7f7089b053912a
rhnsct <rhnsct@gmail.com>
2021-04-07 15:10:15 +0300

    commit coffee machine project

---
+++ B3. Coffee Machine/dictionaries.py
@@ -0,0 +1,27 @@
+MENU = {
+    'Espresso': {
+        'water': 50,
+        'coffee': 18,
+        'milk': 0,
+        'price': 1.50,
+    },
+    'Latte': {
+        'water': 200,
+        'coffee': 24,
+        'milk': 150,
+        'price': 2.50,
+    },
+    'Cappuccino': {
+        'water': 250,
+        'coffee': 24,
+        'milk': 100,
+        'price': 3.00,
+    }
+}
+
+resources = {
+    'water': 300,
+    'coffee': 100,
+    'milk': 200,
+}
+
