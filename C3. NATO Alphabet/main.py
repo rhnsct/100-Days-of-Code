b95d92b0c756355c5be0c77e59b4c1f590bc9209
rhnsct <rhnsct@gmail.com>
2021-05-04 14:20:06 +0200

    Push NATO alphabet converter

--- C3. NATO Alphabet/main.py
+++ C3. NATO Alphabet/main.py
@@ -9,3 +9,4 @@ alphabet = {row.letter: row.code for (index, row) in data_alphabet.iterrows()}
 question = input("Enter a word: ").upper()
 word_list = ' '.join([alphabet[letter] for letter in question if letter != " "])
 print(word_list)
+
