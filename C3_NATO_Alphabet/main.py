04f05a08e044906fc4cf21cde8a4c2738eba5e5e
rhnsct <rhnsct@gmail.com>
2021-05-10 17:43:10 +0200

    validate errors

--- C3_NATO_Alphabet/main.py
+++ C3_NATO_Alphabet/main.py
@@ -4,9 +4,20 @@ import pandas
 # Create dictionary from csv
 data_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet = {row.letter: row.code for (index, row) in data_alphabet.iterrows()}
-
+1231
 # Convert input to alphabet
-question = input("Enter a word: ").upper()
-word_list = ' '.join([alphabet[letter] for letter in question if letter != " "])
-print(word_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        word_list = [alphabet[letter] for letter in word]
+    except KeyError:
+        print("The word must only contain letters in the alphabet.")
+        generate_phonetic()
+    else:
+        print(word_list)
+        
+        
+    
+generate_phonetic()
 
