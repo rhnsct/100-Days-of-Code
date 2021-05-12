61484d90b104f6d5251e26d7c1432e29bc1adab4
rhnsct <rhnsct@gmail.com>
2021-05-12 15:03:02 +0200

    Flashcard application commit

--- C3_NATO_Alphabet/main.py
+++ C3_NATO_Alphabet/main.py
@@ -4,7 +4,7 @@ import pandas
 # Create dictionary from csv
 data_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet = {row.letter: row.code for (index, row) in data_alphabet.iterrows()}
-1231
+
 # Convert input to alphabet
 
 def generate_phonetic():
